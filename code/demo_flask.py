232d769174cdc2fd2e0557fd408fcf233fa1e747
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2019-04-01 05:45:07 -0600

    Updated Flask demo.

--- code/demo_flask.py
+++ code/demo_flask.py
@@ -1,16 +1,38 @@
+# ===============================================================
+# Author: Rodolfo Ferro
+# Email: ferro@cimat.mx
+# Twitter: @FerroRodolfo
+#
+# ABOUT COPYING OR USING PARTIAL INFORMATION:
+# This script was originally created by Rodolfo Ferro, for
+# his workshop in HackSureste 2019 at Universidad Modelo
+# in Mérida. Any explicit usage of this script or its
+# contents is granted according to the license provided and
+# its conditions.
+# ===============================================================
+
+# -*- coding: utf-8 -*-
+
 from flask import Flask
+from reddit_news_reader import reddit_headlines
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def url_principal():
-    return "Hello world!"
+    return "<h1>Hello world!</h1>"
 
 
 @app.route('/pepe/')
 def url_pepe():
     return "Hola soy pepe."
+
+
+@app.route('/reddit/<int:n>')
+def url_number(n):
+    titles = reddit_headlines('artificial', n)
+    return "<h1>Títulos:</h1><p>{}</p>".format(titles)
 
 
 if __name__ == '__main__':
